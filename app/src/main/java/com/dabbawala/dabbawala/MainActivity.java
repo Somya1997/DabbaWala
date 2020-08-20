5d4b5b8110705203379f6621a5c517e075acab98
SidMnnit <sidd.mnnit@gmail.com>
2020-08-20 21:47:01 +0530

    Register Layout Done

--- app/src/main/java/com/dabbawala/dabbawala/MainActivity.java
+++ app/src/main/java/com/dabbawala/dabbawala/MainActivity.java
@@ -2,13 +2,25 @@ package com.dabbawala.dabbawala;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnSignIn;
+    Button btnRegister;
+
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        btnRegister = (Button) findViewById(R.id.btnRegister)
     }
 }
