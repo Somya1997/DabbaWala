7afed0f9d1916fcca963cd0491973d5ddd546dff
SidMnnit <sidd.mnnit@gmail.com>
2020-08-21 14:06:34 +0530

    Driver location done

--- app/src/main/java/com/dabbawala/dabbawala/Welcome.java
+++ app/src/main/java/com/dabbawala/dabbawala/Welcome.java
@@ -3,17 +3,24 @@ package com.dabbawala.dabbawala;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
+import android.widget.Toast;
 
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
@@ -22,6 +29,7 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApi;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.internal.GoogleApiAvailabilityCache;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -38,20 +46,21 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Welcome extends FragmentActivity implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
+import java.util.List;
+import java.util.Locale;
+
+public class Welcome extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    LocationManager locationManager;
+    LocationListener locationListener;
     //PLay Services
     private static final int MY_PERMISSION_REQUEST_CODE=7000;
     private static final int PLAY_SERVICES_RES_REQUEST=7001;
 
     private LocationRequest mLocationRequest;
-    private GoogleApiClient mGoogleApiClient;
-    private Location mLastLocation;
+    private Location lastKnownLocation;
 
     private static int UPDATE_INTERVAL=5000;
     private static int FASTEST_INTERVAL=3000;
@@ -75,27 +84,7 @@ public class Welcome extends FragmentActivity implements OnMapReadyCallback,
 
         //Init view
         location_switch=(MaterialAnimatedSwitch)findViewById(R.id.location_switch);
-        location_switch.setOnCheckedChangeListener(new MaterialAnimatedSwitch.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(boolean isChecked) {
-                if(isChecked){
-                    startLocationUpdates();
-                    displayLocation();
-                    Snackbar.make(mapFragment.getView(),"You are online",Snackbar.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    stopLocationUpdates();
-                    mCurrent.remove();
-                    Snackbar.make(mapFragment.getView(),"You are offline",Snackbar.LENGTH_SHORT).show();
-                }
-            }
-        });
 
-        drivers= FirebaseDatabase.getInstance().getReference("Drivers");
-        geoFire=new GeoFire(drivers);
-
-        setUpLocation();
     }
 
     private void setUpLocation() {
@@ -117,40 +106,41 @@ public class Welcome extends FragmentActivity implements OnMapReadyCallback,
         }
         else
         {
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, (com.google.android.gms.location.LocationListener) this);
+           locationManager.removeUpdates(locationListener);
 
         }
     }
 
     private void displayLocation() {
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
-        {
-            return;
-        }
-        mLastLocation=LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if(mLastLocation!=null){
-            if(location_switch.isChecked()){
-                final double latitude=mLastLocation.getLatitude();
-                final double longitude=mLastLocation.getLongitude();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-                //Update to Firebase
-                geoFire.setLocation(FirebaseAuth.getInstance().getCurrentUser().getUid(), new GeoLocation(latitude, longitude), new GeoFire.CompletionListener() {
-                    @Override
-                    public void onComplete(String key, DatabaseError error) {
-                        //Add Marker
-                        if(mCurrent!=null)
-                            mCurrent.remove();
-                        mCurrent=mMap.addMarker(new MarkerOptions()
-                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.car))
-                                .position(new LatLng(latitude,longitude))
-                                .title("Your Location"));
-                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,longitude),15f));
+            if (lastKnownLocation != null) {
+                if (location_switch.isChecked()) {
+                    final double latitude = lastKnownLocation.getLatitude();
+                    final double longitude = lastKnownLocation.getLongitude();
 
-                        //Draw animation Rotation
-                        rotateMarker(mCurrent,-360,mMap);
-                    }
-                });
+                    //Update to Firebase
+                    geoFire.setLocation(FirebaseAuth.getInstance().getCurrentUser().getUid(), new GeoLocation(latitude, longitude), new GeoFire.CompletionListener() {
+                        @Override
+                        public void onComplete(String key, DatabaseError error) {
+                            //Add Marker
+                            if (mCurrent != null)
+                                mCurrent.remove();
+                            mCurrent = mMap.addMarker(new MarkerOptions()
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.car))
+                                    .position(new LatLng(latitude, longitude))
+                                    .title("Your Location"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 15f));
+
+                            //Draw animation Rotation
+                            rotateMarker(mCurrent, -360, mMap);
+                        }
+                    });
+                }
             }
         }
     }
@@ -158,7 +148,7 @@ public class Welcome extends FragmentActivity implements OnMapReadyCallback,
     private void rotateMarker(final Marker mCurrent, final float i, GoogleMap mMap) {
         final Handler handler=new Handler();
         final long start= SystemClock.uptimeMillis();
-        final float startRoation=mCurrent.getRotation();
+        final float startRotation=mCurrent.getRotation();
         final long duration=1500;
 
         final Interpolator interpolator=new LinearInterpolator();
@@ -167,7 +157,7 @@ public class Welcome extends FragmentActivity implements OnMapReadyCallback,
             public void run() {
                 long elapsed=SystemClock.uptimeMillis()-start;
                 float t=interpolator.getInterpolation((float)elapsed/duration);
-                float rot=t*i+(1-t)*startRoation;
+                float rot=t*i+(1-t)*startRotation;
                 mCurrent.setRotation(-rot>180?rot/2:rot);
                 if(t<1.0){
                     handler.postDelayed(this,16);
@@ -178,49 +168,38 @@ public class Welcome extends FragmentActivity implements OnMapReadyCallback,
 
     }
 
-    private void startLocationUpdates() {
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
-        {
-            return;
-        }
-        else
-        {
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest,this);
-
-        }
-    }
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                mMap.clear();
+                lastKnownLocation = location;
+                displayLocation();
+            }
 
-    }
+        };
+        location_switch.setOnCheckedChangeListener(new MaterialAnimatedSwitch.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(boolean isChecked) {
+                if(isChecked){
+                    displayLocation();
+                    Snackbar.make(mapFragment.getView(),"You are online",Snackbar.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    stopLocationUpdates();
+                    mCurrent.remove();
+                    Snackbar.make(mapFragment.getView(),"You are offline",Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
 
-    @Override
-    public void onLocationChanged(@NonNull Location location) {
-        mLastLocation=location;
-        displayLocation();
+        drivers= FirebaseDatabase.getInstance().getReference("DriversLocation");
+        geoFire=new GeoFire(drivers);
 
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        displayLocation();
-        startLocationUpdates();
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        mGoogleApiClient.connect();
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-
+        setUpLocation();
     }
 }
